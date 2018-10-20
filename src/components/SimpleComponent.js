154d49bc84caa263dacec03710b8110513c19735
Veronica Bustabad <vbustabad@gmail.com>
2018-10-20 23:33:38 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -11,21 +11,15 @@ class SimpleComponent extends React.Component {
 
   handleClick = event => {
     if (this.state.mood === "happy") {
-     this.setState({
-       mood: "sad"
-     })
+     this.setState({mood: "sad"})
     } else {
-      this.setState({
-        mood: "happy"
-      })
+     this.setState({mood: "happy"})
     }
   }
 
   render() {
     return (
-      <div className="current-mood" onClick={this.handleClick}>
-        {this.state.mood}
-      </div>
+      <div className="current-mood" onClick={this.handleClick}>{this.state.mood}</div>
     );
   }
 }
