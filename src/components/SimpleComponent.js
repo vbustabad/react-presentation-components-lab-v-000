189d66b2c6fad65b64a648addd3152119768fa8c
Veronica Bustabad <vbustabad@gmail.com>
2018-10-20 23:33:11 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
 class SimpleComponent extends React.Component {
-<<<<<<< HEAD
   constructor() {
     super();
-=======
-  constructor(props) {
-    super(props);
->>>>>>> 5874cc140185fc80754f504efe120362ab5e5731
 
     this.state = {
       mood: "happy"
@@ -15,33 +10,16 @@ class SimpleComponent extends React.Component {
   }
 
   handleClick = event => {
-<<<<<<< HEAD
     if (this.state.mood === "happy") {
-     this.setState({
-       mood: "sad"
-     })
+     this.setState({mood: "sad"})
     } else {
-      this.setState({
-        mood: "happy"
-      })
+     this.setState({mood: "happy"})
     }
   }
-=======
-   this.setState({
-     mood: "sad"
-   })
- }
->>>>>>> 5874cc140185fc80754f504efe120362ab5e5731
 
   render() {
     return (
-      <div className="current-mood" onClick={this.handleClick}>
-<<<<<<< HEAD
-=======
-        <h3> Current Mood: </h3>
->>>>>>> 5874cc140185fc80754f504efe120362ab5e5731
-        {this.state.mood}
-      </div>
+      <div className="current-mood" onClick={this.handleClick}>{this.state.mood}</div>
     );
   }
 }
